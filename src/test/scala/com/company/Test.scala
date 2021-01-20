7d8caa900132e597c30bbae664c6ae16f71c8c30
Vladyslav Vinnyk <bagok286@gmail.com>
2021-01-20 11:35:52 +0200

    Refactored.

--- src/test/scala/com/company/Test.scala
+++ src/test/scala/com/company/Test.scala
@@ -1,44 +1,42 @@
 package com.company
 
-import com.company.balancer.LoadBalancer
-
 object Test {
-  private val lb = LoadBalancer()
+//  private val lb = AkkaLoadBalancer()
 
   def main(args: Array[String]): Unit = {
-    //    val rq = List(
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //      HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
-    //    )
-    //
-    //    val promises = rq.map(_._2)
-    //    rq.foreach(a => LoadBalancer().queue.offer(a))
-    //    Thread.sleep(5.seconds.toMillis)
-    //    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    //    promises.foreach(p => println(p.future))
-    //    Thread.sleep(5.seconds.toMillis)
-    //    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    //    promises.foreach(p => println(p.future))
-    //    Thread.sleep(5.seconds.toMillis)
-    //    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    //    promises.foreach(p => println(p.future))
-    //    Thread.sleep(5.seconds.toMillis)
-    //    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    //    promises.foreach(p => println(p.future))
+//        val rq = List(
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse],
+//          HttpRequest(uri = "/get-fortune") -> Promise[HttpResponse]
+//        )
+//
+//        val promises = rq.map(_._2)
+//        rq.foreach(a => LoadBalancer().queue.offer(a))
+//        Thread.sleep(30.seconds.toMillis)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+//        promises.foreach(p => println(p.future))
+//        Thread.sleep(5.seconds.toMillis)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+//        promises.foreach(p => println(p.future))
+//        Thread.sleep(5.seconds.toMillis)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+//        promises.foreach(p => println(p.future))
+//        Thread.sleep(5.seconds.toMillis)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+//        promises.foreach(p => println(p.future))
   }
 
 }
